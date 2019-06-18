761fcfea39f8ae61126517de1c3466da9e4d636d
bitcamp30 <bitcamp30@bitcamp>
2019-06-18 17:12:05 +0900

    Spring setting to MySQL

---
+++ sp1/src/main/java/org/zerock/controller/HomeController.java
@@ -0,0 +1,49 @@
+package org.zerock.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.zerock.service.HelloService;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	// 이 타입의 빈이 있으면 의존성을 주입해줘 라는 의미이다.
+	@Autowired
+	private HelloService service;
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+
+		logger.info("----------------------------------");
+		logger.info("----------------------------------");
+		logger.info("----------------------------------");
+		logger.info("----------------------------------");
+		logger.info(""+service);
+		logger.info("----------------------------------");
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+}
